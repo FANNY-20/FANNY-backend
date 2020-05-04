6bc7b3c94b00be4dbcab60f78d80e24cd1c7f3bf
Julien Roche <julien.roche@soyhuce.fr>
2020-05-04 18:05:43 +0200

    Remove binding model route

--- app/App/Providers/RouteServiceProvider.php
+++ app/App/Providers/RouteServiceProvider.php
@@ -2,11 +2,9 @@
 
 namespace App\Providers;
 
-use Domain\Geolocation\Models\Geolocation;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Route;
 use Soyhuce\ModelInjection\BindModels;
-use Soyhuce\Rules\DbRules;
 
 class RouteServiceProvider extends ServiceProvider
 {
@@ -15,13 +13,6 @@ class RouteServiceProvider extends ServiceProvider
     public function boot(): void
     {
         parent::boot();
-
-        $this->bindModels();
-    }
-
-    protected function bindModels(): void
-    {
-        $this->bindModel('geolocation', Geolocation::class, rules(DbRules::string()));
     }
 
     public function map(): void
