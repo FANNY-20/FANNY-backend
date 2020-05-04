99eeb2733c49a9e66ff093c4a9774f6d17a98b82
Julien Roche <julien.roche@soyhuce.fr>
2020-05-04 15:44:05 +0200

    feat(Meet): Create a migration `Meet` and create model/relationships

--- app/Domain/Geolocation/Models/Geolocation.php
+++ app/Domain/Geolocation/Models/Geolocation.php
@@ -3,13 +3,16 @@
 namespace Domain\Geolocation\Models;
 
 use Domain\Geolocation\Casts\LocationCast;
+use Domain\Meet\Meet;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 /**
  * @property string $uuid
  * @property \Domain\Geolocation\Models\Point $location
  * @property \Illuminate\Support\Carbon $created_at
  * @property \Illuminate\Support\Carbon $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection<\Domain\Meet\Meet> $meets
  */
 class Geolocation extends Model
 {
@@ -23,4 +26,9 @@ class Geolocation extends Model
     protected $casts = [
         'location' => LocationCast::class,
     ];
+
+    public function meets(): HasMany
+    {
+        return $this->hasMany(Meet::class, 'geolocation_from');
+    }
 }
--- database/factories/GeolocationFactory.php
+++ database/factories/GeolocationFactory.php
@@ -4,10 +4,19 @@
 
 use Domain\Geolocation\Models\Geolocation;
 use Domain\Geolocation\Models\Point;
+use Domain\Meet\Meet;
+use Faker\Generator as Faker;
 
-$factory->define(Geolocation::class, static function (Faker\Generator $faker) {
+$factory->define(Geolocation::class, static function (Faker $faker) {
     return [
         'uuid' => $faker->unique()->sha256,
         'location' => new Point($faker->latitude, $faker->longitude),
     ];
 });
+$factory->afterCreatingState(
+    Geolocation::class,
+    'with-meets',
+    static function (Geolocation $geolocation, Faker $faker) {
+        factory(Meet::class)->create(['geolocation_from' => $geolocation]);
+    }
+);
