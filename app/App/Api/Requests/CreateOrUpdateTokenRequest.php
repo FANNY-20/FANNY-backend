c8c3b73c446f7b65b31c1434da97ed277d58a37b
Julien Roche <julien.roche@soyhuce.fr>
2020-05-07 11:02:33 +0200

    feat(Token): Expose route to save many tokens

---
+++ app/App/Api/Requests/CreateOrUpdateTokenRequest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Api\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Soyhuce\Rules\DbRules;
+
+/**
+ * @property-read array<string> $tokens
+ */
+class CreateOrUpdateTokenRequest extends FormRequest
+{
+    /**
+     * @return array<string, string>
+     */
+    public function rules(): array
+    {
+        return [
+            'tokens' => rules(['required', 'array']),
+            'tokens.*' => rules(['required', DbRules::string()]),
+        ];
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
 use App\Api\Controllers\GeolocationController;
+use App\Api\Controllers\TokenController;
 use Support\Http\Middleware\AuthenticateApp;
 
 Route::group(['middleware' => AuthenticateApp::class], static function (): void {
     Route::post('geolocations', [GeolocationController::class, 'store']);
+
+    Route::post('tokens', [TokenController::class, 'store']);
 });
