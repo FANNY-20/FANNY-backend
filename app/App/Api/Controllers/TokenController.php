24bbcf70c234e5d4384f0b2728e2be6828271972
Julien Roche <julien.roche@soyhuce.fr>
2020-05-07 11:16:23 +0200

    feat(Token): expose route to retrieve all tokens

--- app/App/Api/Controllers/TokenController.php
+++ app/App/Api/Controllers/TokenController.php
@@ -3,11 +3,21 @@
 namespace App\Api\Controllers;
 
 use App\Api\Requests\CreateOrUpdateTokenRequest;
+use App\Api\Resources\TokenList;
 use Domain\Token\Actions\CreateOrUpdateTokenAction;
+use Domain\Token\Models\Token;
+use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Http\Response;
 
 class TokenController
 {
+    public function index(): JsonResource
+    {
+        $tokens = Token::all();
+
+        return TokenList::collection($tokens);
+    }
+
     public function store(CreateOrUpdateTokenRequest $request, CreateOrUpdateTokenAction $createAction): Response
     {
         $createAction->execute($request->tokens);
--- routes/api.php
+++ routes/api.php
@@ -7,5 +7,6 @@ use Support\Http\Middleware\AuthenticateApp;
 Route::group(['middleware' => AuthenticateApp::class], static function (): void {
     Route::post('geolocations', [GeolocationController::class, 'store']);
 
+    Route::get('tokens', [TokenController::class, 'index']);
     Route::post('tokens', [TokenController::class, 'store']);
 });
